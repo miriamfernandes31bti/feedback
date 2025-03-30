3fca1c566182f60e74583533e46fcb5f8b0a4cd0
miriamfernandessilvaaa <miriamfernandessilvaaa@gmail.com>
2025-03-30 18:01:26 +0000

    finalização do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,67 @@
-import iconStar from "./assets/icon-star.svg"
-import { useState } from "react"
+import iconStar from "./assets/icon-star.svg";
+import illustrationThankYou from "./assets/illustration-thank-you.svg";
+import { useState } from "react";
 
 export function App() {
-  const[submited, setSubmited] = useState(false)
+  const [notaAvaliacao, setNotaAvaliacao] = useState(0);
+  const [submited, setSubmited] = useState(false);
+
+  function handleMudarNotaAvaliacao(nota) {
+    setNotaAvaliacao(nota);
+  }
 
   function handleSubmit() {
-    if(rateNote !== 0) {
-      setSubmited(!submited)
-      return
+    if (notaAvaliacao !== 0) {
+      setSubmited(true);
+      return;
     }
 
-    alert("Please, choose a note!")
+    alert("Please, choose a note!");
   }
 
-  function handleChangeRateNote(value) {
-    setRateNote(value)
-
-  }
-
-  return(
-      submited ===  false ? (
-        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass">
-        
-          <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
-            <img src={iconStar} alt="iconStar" />
-          </div>
-
-          <h1 className="font-bold text-2xl mb-2.5">How did we do?</h1>
-          <p className="text-light-gray text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated 
-            to help us improve our offering!</p>
-
-          <div className="flex justify-between mb-6">
-            <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray font-bold focus:bg-medium-gray
-             focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(1)}/>
-            <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray font-bold focus:bg-medium-gray
-             focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(2)}/>
-            <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray font-bold focus:bg-medium-gray
-             focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(3)}/>
-            <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray font-bold focus:bg-medium-gray
-             focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(4)}/>
-            <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray font-bold focus:bg-medium-gray 
-             focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(5)}/>
-          </div>
-
-          <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1 hover:bg-white hover:text-orange cursor-pointer
-          transition duration-400" onClick={handleSubmit}>Submit</button>
+  return (
+    submited === false ? (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
+        <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
+          <img src={iconStar} alt="icon star" />
         </div>
-      ) : (
-        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass text-center">
+
+        <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
+
+        <p className="text-sm text-light-grey mb-6 leading-1">
+          Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+        </p>
+
+        <div className="flex justify-between mb-6">
+          {[1, 2, 3, 4, 5].map((nota) => (
+            <input
+              key={nota}
+              type="button"
+              className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white hover:cursor-pointer"
+              onClick={() => handleMudarNotaAvaliacao(nota)}
+            />
+          ))}
         </div>
-      )
-  )
+
+        <button
+          onClick={handleSubmit}
+          className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3 hover:bg-white hover:text-orange hover:cursor-pointer"
+        >
+          Submit
+        </button>
+      </div>
+    ) : (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass max-w-103">
+        <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration Thank You" />
+
+        <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-2xl mb-6">
+          You selected {notaAvaliacao} out of 5
+        </p>
+        <h1 className="text-2xl font-bold mb-2.5 text-center">Thank You!</h1>
+        <p className="text-sm text-light-grey leading-1 text-center">
+          We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
+        </p>
+      </div>
+    )
+  );
 }
